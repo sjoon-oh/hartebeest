df860e9c67f679e7dc03706857f97b3ad122c29c
sjoon-oh <sjoon.pm@gmail.com>
2023-08-03 11:55:01 +0900

    Fix: allocation critical

--- src/hb_alloc.cc
+++ src/hb_alloc.cc
@@ -6,7 +6,7 @@
 #include "./includes/hb_alloc.hh"
 
 uint8_t* hartebeest::alloc_buffer(size_t len, int align) {
-    return reinterpret_cast<uint8_t*>(aligned_alloc(len, align));
+    return reinterpret_cast<uint8_t*>(aligned_alloc(align, len));
 }
 
 void hartebeest::free_buffer(uint8_t* buf) {
